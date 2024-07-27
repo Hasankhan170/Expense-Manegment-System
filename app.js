2f99cee99eea5a1eeaee830944c7f991f5b619c9
Hasankhan170 <hasankhanali0316@gmail.com>
2024-07-27 01:28:52 -0700

    some func complete

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const form = document.querySelector('form')
+const p = document.querySelector('p')
+const expense = document.querySelector('.expense')
+const amount = document.querySelector('.amount')
+
+const expenseShow = document.querySelector('.expense-td')
+const amountShow = document.querySelector('.amount-td')
 
 
+
+let arr = []
 
 form.addEventListener('submit' , (e)=>{
     e.preventDefault();
-    const expense = document.querySelector('.expense').value
-    const amount = document.querySelector('.amount').value
 
-    console.log(expense);
-    console.log(amount);
+
+    console.log(expense.value);
+    console.log(amount.value);
+
+    expenseShow.innerHTML = `${expense.value}`
+    amountShow.innerHTML = `${amount.value}`
+
+
+    if(expense === '' || amount === ''){
+        alert('Please fill all fields');
+        return;
+    }
+
+    arr.push({expense,amount})
+    expense.value = ''
+    amount.value = ''
+    form.reset
 
 }) 
