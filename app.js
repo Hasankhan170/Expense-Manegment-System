1561b67848e91468ebe3e42cd9eca5d917b01aa6
Hasankhan170 <hasankhanali0316@gmail.com>
2024-07-27 02:26:43 -0700

    error solved

--- app.js
+++ app.js
@@ -1,91 +1,44 @@
 const form = document.querySelector('form')
 const expense = document.querySelector('.expense')
 const amount = document.querySelector('.amount')
-const tbody = document.querySelector('tbody')
+const showValue = document.querySelector('tbody')
 let arr = []
 
 form.addEventListener('submit' , (e)=>{
     e.preventDefault();
 
-    console.log(expense.value);
-    console.log(amount.value);
+    const expenseShow = expense.value.trim()
+    const amountShow = amount.value.trim()
+
+    console.log(expenseShow);
+    console.log(amountShow);
 
     if(expense === '' || amount === ''){
         alert('Please fill all fields');
         return;
     }
 
-    arr.push({expense,amount})
+    arr.push({expenseShow,amountShow})
 
     expense.value = ''
     amount.value = ''
 
-    renderEntries()
+    rendarEntries()
 }) 
 
-// function rendarEntries(){
+function rendarEntries(){
 
-//     showValue.innerHTML = '';
+    showValue.innerHTML = '';
 
-//     arr.forEach((entry)=>{
-//         const row = `
-//             <tr>
-//                 <td data-label="expense">${entry.expense}</td>
-//                 <td data-label="amount">${entry.amount}</td>
-//             </tr>
-//         `;
-//         showValue.innerHTML += row;
-//     });
-// }
-
-function renderEntries() {
-
-    tbody.innerHTML = '';
-
-   
-    arr.forEach((entry, index) => {
+    arr.forEach((entry)=> {
         const row = `
             <tr>
-                <td data-label="Expense">${entry.expense}</td>
-                <td data-label="Amount">${entry.amount}</td>
-                <td data-label="Delete"><button onclick="deleteEntry(${index})">Delete</button></td>
-                <td data-label="Edit"><button onclick="editEntry(${index})">Edit</button></td>
+                <td data-label="Expense">${entry.expenseShow}</td>
+                <td data-label="Amount">${entry.amountShow}</td>
             </tr>
         `;
-        tbody.innerHTML += row;
+        showValue.innerHTML += row;
     });
 }
-
-
-
-
-// const form = document.querySelector('form');
-// const expenseInput = document.querySelector('.expense');
-// const amountInput = document.querySelector('.amount');
-
-// const tbody = document.querySelector('tbody');
-// let arr = [];
-
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-
-//     const expense = expenseInput.value.trim();
-//     const amount = amountInput.value.trim();
-
-//     if (expense === '' || amount === '') {
-//         alert('Please fill all fields');
-//         return;
-//     }
-
-
-//     arr.push({ expense, amount });
-
-//     expenseInput.value = '';
-//     amountInput.value = '';
-
-//     renderEntries();
-// });
-
-
 
 
