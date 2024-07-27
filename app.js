e25f7219bb50c3649b2761505fd9c1cecb8b3ff6
Hasankhan170 <hasankhanali0316@gmail.com>
2024-07-27 02:42:07 -0700

    almost complete

--- app.js
+++ app.js
@@ -1,44 +1,61 @@
-const form = document.querySelector('form')
-const expense = document.querySelector('.expense')
-const amount = document.querySelector('.amount')
-const showValue = document.querySelector('tbody')
-let arr = []
+const form = document.querySelector('form');
+const expense = document.querySelector('.expense');
+const amount = document.querySelector('.amount');
+const showValue = document.querySelector('tbody');
+let arr = [];
 
 form.addEventListener('submit' , (e)=>{
     e.preventDefault();
 
-    const expenseShow = expense.value.trim()
-    const amountShow = amount.value.trim()
+    const expenseShow = expense.value.trim();
+    const amountShow = amount.value.trim();
 
     console.log(expenseShow);
     console.log(amountShow);
 
-    if(expense === '' || amount === ''){
-        alert('Please fill all fields');
-        return;
-    }
+    // if(expense === '' || amount === ''){
+    //     alert('Please fill all fields');
+    //     return;
+    // }
 
-    arr.push({expenseShow,amountShow})
+    arr.push({expenseShow,amountShow});
 
-    expense.value = ''
-    amount.value = ''
+    expense.value = '';
+    amount.value = '';
 
-    rendarEntries()
-}) 
+    rendarEntries();
+}) ;
 
 function rendarEntries(){
 
     showValue.innerHTML = '';
 
-    arr.forEach((entry)=> {
+    arr.forEach((entry,index)=> {
         const row = `
             <tr>
                 <td data-label="Expense">${entry.expenseShow}</td>
                 <td data-label="Amount">${entry.amountShow}</td>
+                <td><button class = "delete-btn" onclick="deleteEntry(${index})"><i class="fa-solid fa-trash"></i></button></td>
+                <td><button class = "edit-btn" onclick="editEntry(${index})"><i class="fa-solid fa-pen-to-square"></i></button></td>
             </tr>
         `;
         showValue.innerHTML += row;
     });
+};
+
+function deleteEntry(index){
+    arr.splice(index,1)
+    rendarEntries()
 }
 
+function editEntry(index){
 
+    const newExpense = prompt('Update Expense');
+    const newAmount = prompt('Update Amount');
+
+    arr[index].expenseShow = newExpense;
+    arr[index].amountShow = newAmount;
+
+    rendarEntries();
+
+}
